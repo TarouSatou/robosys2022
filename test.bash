2d68b7d9ab062e3d6a5ecfa368b0f449c47e3eaa
Tarou Satou <zabulangdabianzu@gmail.com>
2022-11-03 12:27:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,23 +2,6 @@
 #SPDX-FileCopyrightText:2022 Tarou Sato<s21c1023nb@s.chibakoudai.jp>
 #SPDX-License-Identifier:BSD-3-Clause
 
-ng(){
-      echo NG at Line $1
-      res=1
-}
-res=0
-+ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
-[ "${out}" = 15 ] || ng ${LINENO}
-+ '[' 15 = 15 ']'
-      　 
-[ "$res" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
-exit $res
-+ exit 0
+[ "${out}" = 15 ]
