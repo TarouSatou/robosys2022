78e732349b51c490959d7730136686600ecf6ea4
Tarou Satou <zabulangdabianzu@gmail.com>
2022-11-03 11:51:34 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -3,24 +3,15 @@
 #SPDX-License-Identifier:BSD-3-Clause
 
 ng(){
-	echo NG at Line $1
-	res=1
+      echo NG at Line $1
+      res=1
 }
 res=0
 
-
 out=$(seq 5 | ./plus)
-[ "$out" = 15 ] || ng ${LINENO}
-
-
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo  | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
+[ "${out}" = 14 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
- exit $res
+exit $res
+
+
