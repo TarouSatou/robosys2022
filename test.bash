6d5557fd7186186e242c57f6aed4ea9fc4d82c5d
Tarou Satou <zabulangdabianzu@gmail.com>
2022-11-09 15:40:38 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,9 +2,26 @@
 #SPDX-FileCopyrightText:2022 Tarou Sato<s21c1023nb@s.chibakoudai.jp>
 #SPDX-License-Identifier:BSD-3-Clause
 
+ng () {
+       	echo NG at Line $1
+       	res=1
+}
+
+res=0
+
 ### I/O TEST ###
 out=$(seq 5| ./plus)
 
-[ "${out}" = 15.0 ]
+[ "${out}" = 15.0 ]|| ng ${LINENO}
 
-
+ ### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+           　 
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+		     　 
+[ "$res" = 0 ] && echo OK
+exit $res:
